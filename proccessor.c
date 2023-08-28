eeb4442118d4921f77fe8cb7925fdf9cc7d382ec
evalase219 <chiomaevangeline85@gmail.com>
2023-08-27 17:31:30 -0700

    The proccessor file

--- proccessor.c
+++ proccessor.c
@@ -3,6 +3,9 @@
 /**
  *opcode_processor - monty proccess opcodes
  *@toks: tokens
+ *@buff: the buffer
+ *@mfile: the file
+ *@l: the count
  */
 
 void opcode_processor(char **toks, char *buff, FILE *mfile, int l)
